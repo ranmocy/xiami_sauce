e33dfb706c9ece762f9c20449d0004e6dd291c46
Wanzhang Sheng <ranmocy@gmail.com>
2013-03-17 22:14:31 +0800

    rewrite track class with test

--- lib/xiami_sauce/track.rb
+++ lib/xiami_sauce/track.rb
@@ -2,21 +2,27 @@ require "cgi"
 
 module XiamiSauce
   class Track
-    attr_reader :url, :index, :track, :album, :artist
+    attr_accessor :id, :index, :name, :location, :url
+    attr_accessor :album_id, :album_cover, :album_name
+    attr_accessor :artist_id, :artist_name
+    # user_id is who use the widget, could be 0.
+    # attr_accessor :user_id, :width, :height, :widgetCode
 
+    # @todo I wish index could be fetch automatically from somewhere.
     def initialize(song_url, index=nil)
-      @song_id = song_url.match(/song\/([0-9]+)/)[1]
-      @index = index
+      @id = song_url.match(/song\/([0-9]+)/)[1]
+      @index = index.to_s
 
       parse_info
     end
 
     def file_name
-      "[#{artist}-#{album}]#{index.to_s.rjust(2, '0')}.#{track}.mp3"
+      index_string = index ? index.to_s.rjust(2, '0') + '.' : ''
+      "[#{artist_name}-#{album_name}]#{index_string}#{name}.mp3"
     end
 
     def info_src
-      @info_src ||= "http://www.xiami.com/widget/xml-single/uid/0/sid/#{@song_id}"
+      @info_src ||= "http://www.xiami.com/widget/xml-single/uid/0/sid/#{id}"
     end
 
 
@@ -24,25 +30,40 @@ module XiamiSauce
 
     def parse_info
       url_str = URI.parse(info_src)
-      site = Net::HTTP.new(url_str.host, url_str.port)
-      xml = site.get2(url_str.path,{'accept'=>'text/html'}).body
-      doc = Nokogiri::XML(xml)
-      @track = doc.at_css("song_name").content
-      @album = doc.at_css("album_name").content
-      @artist = doc.at_css("artist_name").content
-      location = doc.at_css("location").content
-      @url = sospa(location)
+      site    = Net::HTTP.new(url_str.host, url_str.port)
+      xml     = site.get2(url_str.path,{'accept'=>'text/html'}).body
+      doc     = Nokogiri::XML(xml)
+
+      # Song
+      @name        = doc.at_css('song_name').content
+      @location    = doc.at_css('location').content
+      @url         = sospa(@location)
+
+      # Album
+      @album_id    = doc.at_css('album_id').content
+      @album_cover = doc.at_css('album_cover').content
+      @album_name  = doc.at_css('album_name').content
+
+      # Artist
+      @artist_id   = doc.at_css('artist_id').content
+      @artist_name = doc.at_css('artist_name').content
+
+      # Widget
+      # @user_id     = doc.at_css('user_id').content
+      # @width       = doc.at_css('width').content
+      # @height      = doc.at_css('height').content
+      # @widgetCode  = doc.at_css('widgetCode').content
     end
 
     def sospa(location)
-      totle = location.to_i
+      totle   = location.to_i
       new_str = location[1..-1]
-      chu = (new_str.length.to_f / totle).floor
-      yu = new_str.length % totle
-      stor = []
+      chu     = (new_str.length.to_f / totle).floor
+      yu      = new_str.length % totle
+      stor    = []
 
       i = 0
-      while i<yu do
+      while i < yu do
         index = (chu+1)*i
         length = chu+1
         stor[i] = new_str[index...index+length]
@@ -52,7 +73,7 @@ module XiamiSauce
 
 
       i = yu
-      while i<totle do
+      while i < totle do
         index = chu*(i-yu)+(chu+1)*yu
         length = chu
 
--- spec/xiami_sauce/track_spec.rb
+++ spec/xiami_sauce/track_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
 describe XiamiSauce::Track do
-  it "should create from url" do
+  let(:song_url) { "http://www.xiami.com/song/1770361274" }
+  let(:index) { 9 }
 
-  end
+  subject { XiamiSauce::Track.new(song_url, index) }
+
+  its('id') { should == '1770361274' }
+  its('index') { should == '9' }
+  its('name') { should == 'Kids (Unedited Version)' }
+  its('url') { should == 'http://f1.xiami.net/11768/454588/02_1770361274_2491919.mp3' }
+  its('album_id') { should == '454588' }
+  its('album_cover') { should == 'http://img.xiami.com/./images/album/img68/11768/4545881311563810_3.jpg' }
+  its('album_name') { should == 'The Way I Am' }
+  its('artist_id') { should == '11768' }
+  its('artist_name') { should == 'Eminem' }
+
+  its('file_name') { should == '[Eminem-The Way I Am]09.Kids (Unedited Version).mp3' }
 end
-
