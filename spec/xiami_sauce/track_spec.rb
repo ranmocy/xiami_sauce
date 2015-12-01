01f68c0ce8cdd157fdd4d797421381c7f92bdf3c
Ranmocy Sheng <ranmocy@gmail.com>
2015-12-01 09:19:51 -0800

    replace its in rspec

--- spec/xiami_sauce/track_spec.rb
+++ spec/xiami_sauce/track_spec.rb
@@ -6,16 +6,16 @@ describe XiamiSauce::Track do
 
   subject { XiamiSauce::Track.new(song_url, index) }
 
-  its('id') { should == '1770361274' }
-  its('index') { should == '9' }
-  its('name') { should == 'Kids (Unedited Version)' }
-  its('album_id') { should == '454588' }
-  its('album_cover') { should == 'http://img.xiami.com/./images/album/img68/11768/4545881311563810_3.jpg' }
-  its('album_name') { should == 'The Way I Am' }
-  its('artist_id') { should == '11768' }
-  its('artist_name') { should == 'Eminem' }
+  it { subject.id.should == '1770361274' }
+  it { subject.index.should == '9' }
+  it { subject.name.should == 'Kids (Unedited Version)' }
+  it { subject.album_id.should == '454588' }
+  it { subject.album_cover.should == 'http://img.xiami.com/./images/album/img68/11768/4545881311563810_3.jpg' }
+  it { subject.album_name.should == 'The Way I Am' }
+  it { subject.artist_id.should == '11768' }
+  it { subject.artist_name.should == 'Eminem' }
 
-  its('file_name') { should == '[Eminem-The Way I Am]09.Kids (Unedited Version).mp3' }
+  it { subject.file_name.should == '[Eminem-The Way I Am]09.Kids (Unedited Version).mp3' }
 
-  its('url') { should == 'http://f1.xiami.net/11768/454588/02_1770361274_2491919.mp3' }
+  it { subject.url.should == 'http://f1.xiami.net/11768/454588/02_1770361274_2491919.mp3' }
 end
